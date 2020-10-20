e88c3cc144e6f996dd457f8940721e562a1e92de
Arthur Oliveira <arthurpmgb@gmail.com>
2020-10-19 22:56:22 -0300

    Update trim

--- task-edit.php
+++ task-edit.php
@@ -5,13 +5,16 @@ include_once("conexao.php");
 $taskid = filter_input(INPUT_POST, 'taskid', FILTER_SANITIZE_NUMBER_INT);
 $taskname = filter_input(INPUT_POST, 'taskname', FILTER_SANITIZE_STRING);
 $taskdate = filter_input(INPUT_POST, 'taskdate', FILTER_SANITIZE_STRING);
-
+$filtrotask = trim($taskname);
 if(empty($taskdate)){
     $taskdate = 'data indefinida';
 }
-
-$query_edit = "UPDATE tarefas SET tarefa='$taskname', created='$taskdate' WHERE id='$taskid'";
-$result_edit = mysqli_query($conn, $query_edit);
+if(empty($filtrotask)){
+    header("Location: index.php#card-task");
+}else{
+    $query_edit = "UPDATE tarefas SET tarefa='$taskname', created='$taskdate' WHERE id='$taskid'";
+    $result_edit = mysqli_query($conn, $query_edit);
+}
 
 if(mysqli_affected_rows($conn)){
     header("Location: index.php#card-task");
